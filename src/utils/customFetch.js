fb12e2e5e12fd560a8c31daab9a55b1f80d9c3d1
Mohd Saud <mohdsaud1523@gmail.com>
2025-01-09 14:46:19 +0530

    second commit

--- src/utils/customFetch.js
+++ src/utils/customFetch.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const devUrl = "https://notes-sphere-server/api/v1";
+const devUrl = "https://notes-sphere-server.onrender.com/api/v1";
 
 const customFetch = axios.create({
   baseURL: devUrl,
