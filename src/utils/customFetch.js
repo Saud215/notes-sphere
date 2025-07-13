8901e94518fcefba7d409dc54f313278022c3ad8
Saud215 <mohdsaud1523@gmail.com>
2025-07-14 02:27:33 +0530

    added some files related to docker

--- src/utils/customFetch.js
+++ src/utils/customFetch.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-const prodUrl = "https://notes-sphere.onrender.com/api/v1";
+// const prodUrl = "https://notes-sphere.onrender.com/api/v1";
 // const devUrl = "http://localhost:6300/api/v1";
+const prodUrl = `${window.location.origin}/api/v1`;
+// console.log(prodUrl);
 
 const customFetch = axios.create({
   baseURL: prodUrl,
