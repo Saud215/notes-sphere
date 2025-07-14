36216ed1124ef268a3c0a4d2cb13fd879d4c3c28
Saud215 <mohdsaud1523@gmail.com>
2025-07-14 16:48:30 +0530

    made the fetching logic more dynamic

--- src/utils/customFetch.js
+++ src/utils/customFetch.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 // const prodUrl = "https://notes-sphere.onrender.com/api/v1";
 // const devUrl = "http://localhost:6300/api/v1";
-const prodUrl = `${window.location.origin}/api/v1`;
+const origin = window.location.origin;
+const prodUrl = `${origin.substring(0, origin.length - 4) + "6300"}/api/v1`;
+
 // console.log(prodUrl);
 
 const customFetch = axios.create({
